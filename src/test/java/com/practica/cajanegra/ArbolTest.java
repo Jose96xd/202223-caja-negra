d63d59039be39269c05cfd0e249d31748b2dafde
Jose Antonio Martinez <jose96xd@gmail.com>
2022-11-16 10:47:04 +0100

    added some tests

--- src/test/java/com/practica/cajanegra/ArbolTest.java
+++ src/test/java/com/practica/cajanegra/ArbolTest.java
@@ -1,6 +1,7 @@
 package com.practica.cajanegra;
 
 import com.binarytree.BinaryTree;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
@@ -11,14 +12,57 @@ import org.junit.jupiter.api.Disabled;
 
 public class ArbolTest {
 
+    BinaryTree<String> arbolPrueba;
+
+    @BeforeEach
+    public void SetUp(){
+        arbolPrueba = new BinaryTree<String>("1");
+    }
+
+    @Test
+    public void ConstructorTest(){
+        Assertions.assertEquals("1", arbolPrueba.getRoot().getContent(), "En la raiz debería haber un 1");
+    }
+
+    @Test()
+    public void ConstructorTest_Null(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new BinaryTree<String>(null);
+        }, "el constructur con null debería dar una excepción de IllegalArgumentException");
+    }
+
+    @Test
+    public void GetRootTest(){
+        Assert.assertEquals("1", arbolPrueba.getRoot().getContent(),
+                "la raiz debería contener un 1");
+    }
+
+    @Test
+    public void InsertTest(){
+        //arbolPrueba.insert("2", );
+    }
+/*
+    @Test
+    public void RemoveTest(){
+
+    }
 
     @Test
     public void SizeTest() {
-        BinaryTree<String> arbolPrueba = new BinaryTree<String>("1");
         int tamanoReal = arbolPrueba.size();
         Assertions.assertEquals(1, tamanoReal);
     }
 
+
+    @Test
+    public void SizeTest_ArbolInvalido(){
+        BinaryTree<String> arbolPrueba = new BinaryTree<String>("1");
+        ijnfd
+    }
+
+
+
+*/
 
 
 }
